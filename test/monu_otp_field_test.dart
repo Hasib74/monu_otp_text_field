4dca35aac9c65bcb63d99e8c202f9a9ec18f499e
Hasib Akon <hasib15-993@diu.edu.bd>
2023-12-07 15:28:49 +0600

    Otp text field

---
+++ test/monu_otp_field_test.dart
@@ -0,0 +1,13 @@
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:monu_otp_field/monu_otp_field.dart';
+
+void main() {
+  test('adds one to input values', () {
+
+    //widget test
+
+
+
+  });
+}
